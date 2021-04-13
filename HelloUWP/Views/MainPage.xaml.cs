034935ecc5c478c6e1bc8b42f84b9649301906c9
Soomin Shin <ssoomin1@naver.com>
2021-04-13 12:05:55 +0900

    버튼 추가 및 클릭 이벤트 핸들러 구현

--- HelloUWP/Views/MainPage.xaml.cs
+++ HelloUWP/Views/MainPage.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 using HelloUWP.ViewModels;
-
+using Windows.UI.Popups;
 using Windows.UI.Xaml.Controls;
 
 namespace HelloUWP.Views
@@ -13,6 +13,20 @@ namespace HelloUWP.Views
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void Button_ClickAsync(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var myDlg = new MessageDialog("안녕하세요");
+            myDlg.Commands.Add(new UICommand("OK"));
+            myDlg.Commands.Add(new UICommand("아니오"));
+            myDlg.Commands.Add(new UICommand("탈출"));
+            await myDlg.ShowAsync();
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
         }
     }
 }
